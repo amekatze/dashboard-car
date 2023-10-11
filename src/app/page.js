07aa5bb209d3b648ad052f865366879c88e8f4c3
amekatze <merudarko@gmail.com>
2023-10-11 20:51:07 +0900

    header styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,38 @@
 import { Clock } from "@/components/Clock";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="dashboard rounded-3xl flex flex-col justify-between p-4">
+      <div className="dashboard rounded-3xl flex flex-col justify-between p-5">
         <div className="w-full h-16 flex justify-between">
           <div className="flex flex-col">
             <Clock />
           </div>
-          <div className="flex">
-            <div>Battery</div>
-            <div>Bluetooth & Name</div>
+          <div className="flex gap-4 justify-center h-6  items-center ">
+            <div>
+              <Image
+                src="/icons/battery.png"
+                width={24}
+                height={12}
+                alt="Battery"
+                loading="lazy"
+              />
+            </div>
+            <div className="flex gap-1">
+              <div>
+                <Image
+                  src="/icons/Bluetooth.png"
+                  width={18}
+                  height={12}
+                  alt="Battery"
+                  loading="lazy"
+                  className="mt-[2px]"
+                />
+              </div>
+
+              <span>Sam Phone</span>
+            </div>
           </div>
         </div>
         <div className="grid grid-cols-3 grid-rows-4 grow gap-6">
@@ -25,7 +47,7 @@ export default function Home() {
             weather
           </div>
         </div>
-        <div className="w-full h-16">time etc</div>
+        <div className="w-full h-24">time etc</div>
       </div>
     </main>
   );
